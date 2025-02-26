18f4a2221cf47c3bee27900c2c5f816fd5ae8fa4
Francesco Oliva <olibartfast@gmail.com>
2025-02-27 00:00:26 +0100

    added readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # video-classification-inference
 
+
+* Testing video classification using VLM or Vision Transformes models on public models subsets
+
 ### references
 - https://paperswithcode.com/task/video-classification
 - https://paperswithcode.com/task/action-recognition-in-videos
---
+++ get_hmdb51_org.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+wget http://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/hmdb51_org.rar
+
+# contents of hmdb51_org.rar:
+# unrar t hmdb51_org.rar
+
+# UNRAR 7.00 freeware      Copyright (c) 1993-2024 Alexander Roshal
+
+
+# Testing archive hmdb51_org.rar
+
+# Testing     shoot_gun.rar                                             OK 
+# Testing     sit.rar                                                   OK 
+# Testing     situp.rar                                                 OK 
+# Testing     smile.rar                                                 OK 
+# Testing     smoke.rar                                                 OK 
+# Testing     somersault.rar                                            OK 
+# Testing     stand.rar                                                 OK 
+# Testing     swing_baseball.rar                                        OK 
+# Testing     sword.rar                                                 OK 
+# Testing     sword_exercise.rar                                        OK 
+# Testing     talk.rar                                                  OK 
+# Testing     throw.rar                                                 OK 
+# Testing     turn.rar                                                  OK 
+# Testing     walk.rar                                                  OK 
+# Testing     wave.rar                                                  OK 
+# Testing     brush_hair.rar                                            OK 
+# Testing     cartwheel.rar                                             OK 
+# Testing     catch.rar                                                 OK 
+# Testing     chew.rar                                                  OK 
+# Testing     clap.rar                                                  OK 
+# Testing     climb.rar                                                 OK 
+# Testing     climb_stairs.rar                                          OK 
+# Testing     dive.rar                                                  OK 
+# Testing     draw_sword.rar                                            OK 
+# Testing     dribble.rar                                               OK 
+# Testing     drink.rar                                                 OK 
+# Testing     eat.rar                                                   OK 
+# Testing     fall_floor.rar                                            OK 
+# Testing     fencing.rar                                               OK 
+# Testing     flic_flac.rar                                             OK 
+# Testing     golf.rar                                                  OK 
+# Testing     handstand.rar                                             OK 
+# Testing     hit.rar                                                   OK 
+# Testing     hug.rar                                                   OK 
+# Testing     jump.rar                                                  OK 
+# Testing     kick.rar                                                  OK 
+# Testing     kick_ball.rar                                             OK 
+# Testing     kiss.rar                                                  OK 
+# Testing     laugh.rar                                                 OK 
+# Testing     pick.rar                                                  OK 
+# Testing     pour.rar                                                  OK 
+# Testing     pullup.rar                                                OK 
+# Testing     punch.rar                                                 OK 
+# Testing     push.rar                                                  OK 
+# Testing     pushup.rar                                                OK 
+# Testing     ride_bike.rar                                             OK 
+# Testing     ride_horse.rar                                            OK 
+# Testing     run.rar                                                   OK 
+# Testing     shake_hands.rar                                           OK 
+# Testing     shoot_ball.rar                                            OK 
+# Testing     shoot_bow.rar                                             OK 
+# All OK
